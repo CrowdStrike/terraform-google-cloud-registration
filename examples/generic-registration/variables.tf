eb01912342ea68d5e5fa2383f3204f75b1306aaa
Max Zastavnyi <max.zastavnyi@crowdstrike.com>
2025-12-24 10:53:11 -0500

    Fix labels limit validation in examples

--- examples/generic-registration/variables.tf
+++ examples/generic-registration/variables.tf
@@ -184,8 +184,8 @@ variable "labels" {
   }
 
   validation {
-    condition     = length(var.labels) <= 60
-    error_message = "Maximum of 60 custom labels allowed (system labels will be added automatically)."
+    condition     = length(var.labels) <= 64
+    error_message = "Maximum of 64 custom labels allowed."
   }
 }
 
